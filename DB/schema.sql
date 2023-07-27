c306667d01e0bcf37ca0759d87225b3ccc4e6ed4
vagkaratzas <vagelaros.gee@gmail.com>
2023-07-27 09:02:21 +0100

    DB schema init

---
+++ DB/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Proteins (
+    ID VARCHAR(16) PRIMARY KEY,
+    Sequence TEXT NOT NULL
+);
+
+CREATE TABLE Families (
+    ID VARCHAR(16) PRIMARY KEY,
+    MSA BLOB,
+    HMM BLOB,
+    FOREIGN KEY(ID) REFERENCES Proteins(ID)
+);
+
+CREATE TABLE Clusters (
+    ProteinID VARCHAR(16),
+    FamilyID VARCHAR(16),
+    PRIMARY KEY (ProteinID, FamilyID),
+    FOREIGN KEY(ProteinID) REFERENCES Proteins(ID)
+    FOREIGN KEY(FamilyID) REFERENCES Families(ID)
+);
+
+CREATE TABLE Annotations (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    FamilyID VARCHAR(16),
+    Annotation VARCHAR(30) NOT NULL,
+    Description TEXT NOT NULL,
+    Source VARCHAR(30) NOT NULL,
+    FOREIGN KEY(FamilyID) REFERENCES Families(ID)
+);
