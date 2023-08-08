58b205826122260861a99d673acd473193443d42
vagkaratzas <vagelaros.gee@gmail.com>
2023-08-08 15:36:56 +0100

    create families python script refactored and optimized

--- bin/family_rep_into_fasta.py
+++ bin/family_rep_into_fasta.py
@@ -1,50 +1,53 @@
 #!/usr/bin/env python
 
 import sys
-import gzip
 from Bio import SeqIO
 
-def get_matching_cluster_items(cluster_tsv_file, cluster_rep_id):
-    with open(cluster_tsv_file) as f:
-        cluster_items = []
+def get_matching_cluster_ids(clusters_tsv, cluster_rep_id):
+    with open(clusters_tsv) as f:
+        cluster_ids = set()
+        found_cluster = False
         for line in f:
             columns = line.strip().split("\t")
             if columns[0] == cluster_rep_id:
-                cluster_items.append(columns[1])
-    return cluster_items
+                cluster_ids.add(columns[1])
+                found_cluster = True
+            # Break if we've moved past the target cluster
+            elif found_cluster:
+                break
+    
+    return cluster_ids
 
-def filter_sequences(sequence_db_file, cluster_items):
-    with open(sequence_db_file, "rt") as handle:
-        filtered_records = []
+def find_cluster_sequences(input_fasta, cluster_ids):
+    contains_sp = False
+    cluster_seqs = []
+
+    with open(input_fasta, "rt") as handle:
         for record in SeqIO.parse(handle, "fasta"):
-            is_uniprot = False
-            # Check if the header is in Uniprot format
-            if record.id.startswith('sp|'):
-                seq_id = record.id.split('|')[1]  # Split on '|' and take the second element
-                is_uniprot = True
-            else:
-                seq_id = record.id
-            if seq_id in cluster_items:
-                filtered_records.append(record)
-                if is_uniprot:
-                    global contains_sp
+            seq_id = record.id.split('|')[1] if record.id.startswith('sp|') else record.id
+            if seq_id in cluster_ids:
+                cluster_seqs.append(record)
+                if record.id.startswith('sp|'):
                     contains_sp = True
-    return filtered_records
+                cluster_ids.remove(seq_id) # Remove the found item from the remaining set
+                if not cluster_ids: # If no more items are remaining, break
+                    break
+    
+    return contains_sp, cluster_seqs
+
+def write_cluster_sequences(contains_sp, cluster_seqs, output_file):
+    output_file = (contains_sp and "known/" or "unknown/") + output_file
+
+    with open(output_file, "w") as out_handle:
+        SeqIO.write(cluster_seqs, out_handle, "fasta")
+
 
 # Get command line arguments
-cluster_tsv_file = sys.argv[1]
-sequence_db_file = sys.argv[2]
+clusters_tsv = sys.argv[1]
+input_fasta = sys.argv[2]
 output_file = sys.argv[3]
 cluster_rep_id = sys.argv[4]
-contains_sp = False
 
-cluster_items = get_matching_cluster_items(cluster_tsv_file, cluster_rep_id)
-filtered_records = filter_sequences(sequence_db_file, cluster_items)
-
-if not contains_sp:
-    output_file = "unknown/" + output_file
-else:
-    output_file = "known/" + output_file
-
-with open(output_file, "w") as out_handle:
-    SeqIO.write(filtered_records, out_handle, "fasta")
+cluster_ids = get_matching_cluster_ids(clusters_tsv, cluster_rep_id)
+contains_sp, cluster_seqs = find_cluster_sequences(input_fasta, cluster_ids)
+write_cluster_sequences(contains_sp, cluster_seqs, output_file)
